0b633443a8532ed9c0aefb50de3b0b475d14009d
Roger Andersen <hexjelly@hexjelly.com>
2021-12-27 22:14:22 +0100

    wip

--- web/components/event/index.tsx
+++ web/components/event/index.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './event.module.css';
+import { useState } from 'react';
+import { Disclosure, DisclosureButton, DisclosurePanel } from '@reach/disclosure';
 
 export type EventProps = {
   date: string;
@@ -13,16 +15,15 @@ export type EventProps = {
   tags: Array<string>;
 };
 export function Event({ date, eventLink, eventName, imgUrl, organizer, address, facebookLink, tags }: EventProps) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={styles.event}>
       <div className={styles.imgContainer}>
-      <time className={styles.date}>
-        21<span>nov</span>
-      </time>
-      {imgUrl && (
-          <Image className={styles.img} src={imgUrl} alt={`Event ${''}`} layout="fill" />
-          )}
-          </div>
+        <time className={styles.date}>
+          21<span>nov</span>
+        </time>
+        {imgUrl && <Image className={styles.img} src={imgUrl} alt={`Event ${''}`} layout="fill" />}
+      </div>
       <div className={styles.content}>
         <time>time</time>
         <h3>Event name</h3>
@@ -32,16 +33,24 @@ export function Event({ date, eventLink, eventName, imgUrl, organizer, address, 
             Google maps link
           </a>
         </p>
-        <a href="https://facebook.com" className={styles.facebook} target={`_fb_${eventName || 'blank'}`} rel="noreferrer">
-          Facebook link
-        </a>
-        <Link href="/">Event link</Link>
         <ul className={styles.tagList}>
           <li className={styles.tagList__item}>Tag</li>
           <li className={styles.tagList__item}>Tag</li>
           <li className={styles.tagList__item}>Tag</li>
           <li className={styles.tagList__item}>Tag</li>
         </ul>
+        <a
+          href="https://facebook.com"
+          className={styles.eventLink}
+          target={`_fb_${eventName || 'blank'}`}
+          rel="noreferrer"
+        >
+          Facebook link
+        </a>
+        <Disclosure open={isOpen} onChange={() => setIsOpen((prev) => !prev)}>
+          <DisclosurePanel>Event information</DisclosurePanel>
+          <DisclosureButton className={styles.readMore}>{isOpen ? 'Les mindre' : 'Les mer om arrangementet'}</DisclosureButton>
+        </Disclosure>
       </div>
     </div>
   );
--- web/components/layout/index.tsx
+++ web/components/layout/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import { ReactNode, useRef } from 'react';
 
 import styles from './layout.module.css';
-import { DownArrow } from '../icons/down-arrrow';
 
 export type LayoutProps = {
   children: ReactNode;
@@ -15,13 +14,13 @@ export type LayoutProps = {
 export default function Layout({ children, image, title, subTitle }: LayoutProps) {
   const scrollButtonRef = useRef<HTMLButtonElement>(null);
 
-	function scrollToContent() {
-		const buttonDistanceFromTop = scrollButtonRef.current?.offsetTop as number;
-		const buttonHeight = scrollButtonRef.current?.offsetHeight as number;
-		const scrollOffsetFromTop = buttonDistanceFromTop + buttonHeight;
+  function scrollToContent() {
+    const buttonDistanceFromTop = scrollButtonRef.current?.offsetTop as number;
+    const buttonHeight = scrollButtonRef.current?.offsetHeight as number;
+    const scrollOffsetFromTop = buttonDistanceFromTop + buttonHeight;
 
-		window.scrollTo({ top: scrollOffsetFromTop, behavior: "smooth" });
-	}
+    window.scrollTo({ top: scrollOffsetFromTop, behavior: 'smooth' });
+  }
   return (
     <>
       <Head>
@@ -29,30 +28,36 @@ export default function Layout({ children, image, title, subTitle }: LayoutProps
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
       <header className={styles.header}>
+        {image && (
+          <div className={styles.imgContainer}>
+            <Image alt="" src={image} layout="fill" className={styles.backgroundImage} />
+          </div>
+        )}
+        <div className={styles.headerContent}>
 
-        {image && 
-        <div className={styles.imgContainer}>
-        <Image alt="" src={image} layout='fill' />
-        </div>
-        }
-        <div className={styles.container}>
-          <h1>{title}</h1>
-          <p>{subTitle}</p>
+        <div className={styles.topContainer}>
+          <div className={styles.logoContainer}>
+            <Image src="/logo.png" width="272" height="154" alt="Skeivt Kulturår logo" className={styles.logo} />
+          </div>
           <nav aria-label="Main navigation" className={styles.headerNav}>
             <ul>
               <li>
                 <Link href="/">Arrangementer</Link>
               </li>
               <li>
-                <Link href="/submit">Meld inn arrangement</Link>
-              </li>
-              <li>
                 <Link href="/about">Om Skeivt Kulturår</Link>
+              </li>
+              <li className={styles.submitLink}>
+                <Link href="/submit">Meld inn arrangement</Link>
               </li>
             </ul>
           </nav>
         </div>
-        <DownArrow onClick={scrollToContent} ref={scrollButtonRef} />
+        <div className={styles.container}>
+          <h1>{title}</h1>
+          <p>{subTitle}</p>
+        </div>
+        </div>
       </header>
       <main className={styles.main}>{children}</main>
     </>
