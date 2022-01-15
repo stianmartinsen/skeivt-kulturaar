4857839740b837619f40a11e1c2a49b4de20fe06
camorte <carloalfredo.morte@gmail.com>
2022-01-15 20:28:49 +0100

    fix events filtering to show unprocessed events

--- sanity/desk-structure/events.js
+++ sanity/desk-structure/events.js
@@ -1,37 +1,37 @@
-import S from "@sanity/desk-tool/structure-builder";
-import { MdEvent, MdEventAvailable, MdEventBusy } from "react-icons/md";
+import S from '@sanity/desk-tool/structure-builder';
+import { MdEvent, MdEventAvailable, MdEventBusy } from 'react-icons/md';
 
 export default S.listItem()
-	.title("Skeivt Kulturår Events")
-	.icon(MdEvent)
-	.child(
-		S.list()
-			.title("Filters")
-			.items([
-				S.listItem()
-					.title("Waiting for approval")
-					.icon(MdEvent)
-					.child(
-						S.documentTypeList("eventRequest")
-							.title("Events")
-							.filter("_id in path('drafts.**')")
-					),
-				S.divider(),
-				S.listItem()
-					.title("Accepted")
-					.icon(MdEventAvailable)
-					.child(
-						S.documentTypeList("eventRequest")
-							.title("Events")
-							.filter("approved == true")
-					),
-				S.listItem()
-					.title("Rejected")
-					.icon(MdEventBusy)
-					.child(
-						S.documentTypeList("eventRequest")
-							.title("Events")
-							.filter("approved == false")
-					)
-			])
-	);
+  .title('Skeivt Kulturår Events')
+  .icon(MdEvent)
+  .child(
+    S.list()
+      .title('Filters')
+      .items([
+        S.listItem()
+          .title('Waiting for approval')
+          .icon(MdEvent)
+          .child(
+            S.documentTypeList('eventRequest')
+              .title('Events')
+              .filter('_type == "eventRequest" && !defined(approved)')
+          ),
+        S.divider(),
+        S.listItem()
+          .title('Accepted')
+          .icon(MdEventAvailable)
+          .child(
+            S.documentTypeList('eventRequest')
+              .title('Events')
+              .filter('_type == "eventRequest" && approved == true')
+          ),
+        S.listItem()
+          .title('Rejected')
+          .icon(MdEventBusy)
+          .child(
+            S.documentTypeList('eventRequest')
+              .title('Events')
+              .filter('_type == "eventRequest" && approved == false')
+          ),
+      ])
+  );
