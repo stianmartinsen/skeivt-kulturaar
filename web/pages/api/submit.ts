0b242902630cc968934fc0bf769d0666387998d4
Roger Andersen <hexjelly@hexjelly.com>
2022-01-19 12:19:48 +0100

    proof of concept

--- web/pages/api/submit.ts
+++ web/pages/api/submit.ts
@@ -1,20 +1,28 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiResponse } from 'next';
+import { createReadStream } from 'fs'
 
 import middleware from '../../middleware/middleware';
 import nextConnect from 'next-connect';
+import sanity from '../../sanity';
 
 const handler = nextConnect();
 handler.use(middleware);
 
 handler.post(async (req: any, res: NextApiResponse) => {
   console.log(req.body);
-  console.log(req.files);
+  console.log(req.files.image[0])
 
   try {
     // parse form, redirect on success
+    if (req.files.image?.[0]) {
+      const fileStream = createReadStream(req.files.image[0].path);
+      const asset = await sanity.assets.upload("image", fileStream as any, { contentType: req.files.image[0].headers['content-type'], filename: req.files.image[0].originalFilename })
+      console.log(asset);
+    }
     res.redirect(307, '/thanks');
   } catch (err) {
+    console.log(err)
     res.status(500).send({ error: 'failed to fetch data' });
   }
 });
