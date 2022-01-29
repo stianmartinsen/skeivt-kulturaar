8d808271e0024f0bf46230f7415c03ccae534190
Roger Andersen <hexjelly@hexjelly.com>
2022-01-29 13:00:13 +0100

    mvp version of mobile menu (#13)

--- web/components/layout/index.tsx
+++ web/components/layout/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
-import { ReactNode, useRef } from 'react';
+import { ReactNode } from 'react';
+import { slide as Menu } from 'react-burger-menu';
 
 import styles from './layout.module.css';
 
@@ -19,38 +20,57 @@ export default function Layout({ children, image, title, subTitle }: LayoutProps
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
-      <header className={styles.header}>
-        {image && (
-          <div className={styles.imgContainer}>
-            <Image alt="" src={image} layout="fill" className={styles.backgroundImage} />
-          </div>
-        )}
-        <div className={styles.headerContent}>
-          <div className={styles.topContainer}>
-            <div className={styles.logoContainer}>
-              <Image src="/logo.png" width="272" height="154" alt="Skeivt Kulturår logo" className={styles.logo} />
+      <div id="outer-container">
+        <Menu right pageWrapId={'page-wrap'} outerContainerId={'outer-container'} width={400}>
+          <nav aria-label="Main navigation" className={styles.headerNav}>
+            <ul>
+              <li>
+                <Link href="/">Arrangementer</Link>
+              </li>
+              <li>
+                <Link href="/about">Om Skeivt Kulturår</Link>
+              </li>
+              <li className={styles.submitLink}>
+                <Link href="/submit">Meld inn arrangement</Link>
+              </li>
+            </ul>
+          </nav>
+        </Menu>
+        <div id="page-wrap">
+          <header className={styles.header}>
+            {image && (
+              <div className={styles.imgContainer}>
+                <Image alt="" src={image} layout="fill" className={styles.backgroundImage} />
+              </div>
+            )}
+            <div className={styles.headerContent}>
+              <div className={styles.topContainer}>
+                <div className={styles.logoContainer}>
+                  <Image src="/logo.png" width="272" height="154" alt="Skeivt Kulturår logo" className={styles.logo} />
+                </div>
+                <nav aria-label="Main navigation" className={styles.headerNav}>
+                  <ul>
+                    <li>
+                      <Link href="/">Arrangementer</Link>
+                    </li>
+                    <li>
+                      <Link href="/about">Om Skeivt Kulturår</Link>
+                    </li>
+                    <li className={styles.submitLink}>
+                      <Link href="/submit">Meld inn arrangement</Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+              <div className={styles.container}>
+                <h1>{title}</h1>
+                <p>{subTitle}</p>
+              </div>
             </div>
-            <nav aria-label="Main navigation" className={styles.headerNav}>
-              <ul>
-                <li>
-                  <Link href="/">Arrangementer</Link>
-                </li>
-                <li>
-                  <Link href="/about">Om Skeivt Kulturår</Link>
-                </li>
-                <li className={styles.submitLink}>
-                  <Link href="/submit">Meld inn arrangement</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className={styles.container}>
-            <h1>{title}</h1>
-            <p>{subTitle}</p>
-          </div>
+          </header>
+          <main className={styles.main}>{children}</main>
         </div>
-      </header>
-      <main className={styles.main}>{children}</main>
+      </div>
     </>
   );
 }
